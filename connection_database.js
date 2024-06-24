6585f2317890bc0946ad7930792aed6ef844d668
JesusRosml <jesusperezhidalgo16@outlook.com>
2024-06-23 19:11:40 -0600

    Conexión a la base de datos creada, se instalo Express, Mongoose y dotenv

---
+++ connection_database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Conexión exitosa con la base de datos"))
+  .catch((error) =>
+    console.error("Error al conectar con la base de datos:", error)
+  );
+
+mongoose.connection.on("error", (error) =>
+  console.error("Error de conexión con la base de datos:", error)
+);
+
+module.exports = mongoose.connection;
